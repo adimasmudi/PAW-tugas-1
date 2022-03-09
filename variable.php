bdcf1406e7e882a4cc07858afa4a19e8d72246c6
adimasmudi <adimasmudi@gmail.com>
2022-03-09 08:56:31 +0700

    membuat file variable.php

---
+++ variable.php
@@ -0,0 +1,19 @@
+<?php
+
+$angka1 = 10;
+$angka2 = 20;
+$angka3 = 40;
+$angka4 = 16;
+$angka5 = 5;
+
+$sum = $angka1 + $angka3;
+$subtract = $angka2 - $angka5;
+$mul = $angka2*$angka4;
+
+echo "penjumlahan antara ".$angka1." dan ".$angka3." adalah ".$sum;
+echo "<br>";
+echo "Pengurangan antara ".$angka2." dan ".$angka5," adalah ".$subtract;
+echo "<br>";
+echo "Perkalian antara ".$angka2." dan ".$angka4." adalah ".$mul;
+
+?>
